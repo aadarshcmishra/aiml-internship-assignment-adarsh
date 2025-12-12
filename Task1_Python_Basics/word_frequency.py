d205b8432cd8158218eb9c635e23313289837bf0
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:03:22 +0530

    Implement word frequency calculation function
    Adds a function to calculate word frequency in a paragraph.

---
+++ Task1_Python_Basics/word_frequency.py
@@ -0,0 +1,7 @@
+def word_frequency(paragraph):
+    """Calculates word frequency in a paragraph."""
+    words = paragraph.lower().replace('.', '').split()
+    freq_dict = {}
+    for word in words:
+        freq_dict[word] = freq_dict.get(word, 0) + 1
+    return freq_dict
