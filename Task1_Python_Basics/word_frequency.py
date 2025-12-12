f9a9e810cdcb33f834181b8d96735874a6948117
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:27:48 +0530

    Add print statement for word frequency example
    Added a print statement to display the frequency of a sample string.

--- Task1_Python_Basics/word_frequency.py
+++ Task1_Python_Basics/word_frequency.py
@@ -5,3 +5,5 @@ def word_frequency(paragraph):
     for word in words:
         freq_dict[word] = freq_dict.get(word, 0) + 1
     return freq_dict
+  print(f"3. Frequency 'Hello world hello': {word_frequency('Hello world hello')}")
+   
