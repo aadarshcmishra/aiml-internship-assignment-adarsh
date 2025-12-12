7c785394a454b3ac983ceda417875e51102f71e7
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:31:31 +0530

    Update remove_duplicates.py

--- Task1_Python_Basics/remove_duplicates.py
+++ Task1_Python_Basics/remove_duplicates.py
@@ -5,3 +5,4 @@ def remove_duplicates(lst):
         if item not in unique_lst:
             unique_lst.append(item)
     return unique_lst
+print(f"8. Unique [1, 2, 2, 3]: {remove_duplicates([1, 2, 2, 3])}")
