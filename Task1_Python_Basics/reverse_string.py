70e4e749272fd1d595b08a4f1bff7a5a30a74fe9
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:02:32 +0530

    Implement string reversal function
    Adds a function to reverse a string without built-in methods.

---
+++ Task1_Python_Basics/reverse_string.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    """Reverses a string without using built-in reverse()."""
+    reversed_s = ""
+    for char in s:
+        reversed_s = char + reversed_s
+    return reversed_s
