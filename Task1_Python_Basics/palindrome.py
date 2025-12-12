3fdd930055f4ed1f7b2b98d687628097f67a1a14
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:30:15 +0530

    Update palindrome.py

--- Task1_Python_Basics/palindrome.py
+++ Task1_Python_Basics/palindrome.py
@@ -2,3 +2,4 @@ def is_palindrome(data):
     """Checks if a string or number is a palindrome."""
     s = str(data)
     return s == s[::-1]
+print(f"7. Palindrome 'radar': {is_palindrome('radar')}")
