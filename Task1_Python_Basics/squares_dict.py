587041b25d4126a546831752c1cf805a17119fc4
aadarshcmishra <aadarshcmishra@gmail.com>
2025-12-12 21:31:09 +0530

    Add print statement to display squares dictionary

--- Task1_Python_Basics/squares_dict.py
+++ Task1_Python_Basics/squares_dict.py
@@ -1,3 +1,4 @@
 def dict_squares(numbers):
     """Returns dictionary where key=number and value=square."""
     return {num: num**2 for num in numbers}
+print(f"9. Squares [2, 3, 4]: {dict_squares([2, 3, 4])}")
